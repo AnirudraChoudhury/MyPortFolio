3c800292328d2cb513b6bc97c8220b00a99cc428
Anirudra Choudhury <53123185+AnirudraChoudhury@users.noreply.github.com>
2025-06-18 08:40:49 +0000

    Update deployment workflow and enhance CSS handling in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/anirudra-portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/anirudra-portfolio/' : '',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
